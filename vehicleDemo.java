7d2457f28f8e1c92f15931f65c352363509be7a6
Nic <Nic@Nics-MacBook-Air.local>
2015-12-31 17:51:04 -0800

    ch 4

--- vehicleDemo.java
+++ vehicleDemo.java
@@ -1,21 +1,38 @@
-
-class Vehicle{
+// Vehicle class
+class Vehicle {
   int passengers; // # of passengers
   int fuelCap; // fuel capacity in gallons
   int mpg; // fuel consumptionin miles per gallons
+
+// Display the range.
+  void range() {
+    System.out.println("Range is " + fuelCap * mpg);
+  }
 }
 
 
 class vehicleDemo {
   public static void main (String[] args) {
     Vehicle minivan = new Vehicle();
-    int range;
+    Vehicle sportscar = new Vehicle();
 
+    int range1, range2;
+
+    // assign values to fields in minvan
     minivan.passengers = 7;
     minivan.fuelCap = 16;
     minivan.mpg = 21;
 
-    range = minivan.fuelCap * minivan.mpg;
-    System.out.println("Minivan can carry " + minivan.passengers + " with a range of " + range);
+    // assign values to fields in sportscar
+    sportscar.passengers = 2;
+    sportscar.fuelCap = 14;
+    sportscar.mpg = 12;
+
+    System.out.println("Minivan can carry " + minivan.passengers + ". ");
+    minivan.range(); // display range of minivan
+
+    System.out.println("Sportsca can carry " + sportscar.passengers + ". ");
+    sportscar.range(); // display range of minivan
+
   }
 }
