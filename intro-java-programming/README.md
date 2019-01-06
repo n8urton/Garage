8ade86f5f9ebab7ddc5ce61e376a604b16250d34
dokk4381 <johnp4rk3r@gmail.com>
2019-01-05 16:53:23 -0800

    delete old dir

--- intro-java-programming/README.md
+++
@@ -1,3 +0,0 @@
-My first repository! It's really just a place to store .java files I'm creating
-from exercises from "Java Programming; A comprehensive Introduction" by Schildt
-and Skrien. 
--- intro-java-programming/Vehicle.java
+++
@@ -1,8 +0,0 @@
-// Vehicle Class. Used by VehicleDemo
-package javaBook;
-
-  public class Vehicle{
-    int passeners; // # of passengers
-    int fuelCap; // fuel capacity in gallons
-    int mpg; // fuel consumptionin miles per gallons
-  }
